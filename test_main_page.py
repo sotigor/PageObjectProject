675f5a1496e5c251225a50432b1689254230b483
Ihor <sotigor@gmail.com>
2023-01-17 14:24:11 +0200

    should_be_login_link method created

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,11 @@
+from .pages.main_page import MainPage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-#pytest -v --tb=line --language=en test_main_page.py
+#pytest -v --tb=line --language=en-gb test_main_page.py
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
